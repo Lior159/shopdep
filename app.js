5b7a27ede2db22b66d19cc1f2723e6b3b0612889
Lior Zalta <liorzalta24@gmail.com>
2023-04-06 22:37:53 +0300

    modify server settings

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
+
+//settin up ejs
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+//setting up body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//setting up public folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000);
