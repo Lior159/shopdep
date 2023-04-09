2e7ef57527278b4afb797fb0ddf0cfd270192a73
Lior Zalta <liorzalta24@gmail.com>
2023-04-10 00:25:34 +0300

    start working on shop design

--- public/script.js
+++ public/script.js
@@ -1,3 +1,17 @@
+const navbar_account_btn = document.querySelector(".navbar .account-btn");
+const navbar_account_menu = document.querySelector(".navbar .account-menu");
+const navbar_account_arrows = document.querySelectorAll(
+  ".navbar .account-arrow"
+);
+
+navbar_account_btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  navbar_account_menu.classList.toggle("hidden");
+  navbar_account_arrows.forEach((a) => {
+    a.classList.toggle("hidden");
+  });
+});
+
 const infoLbl = document.querySelectorAll(".info-lbl");
 const infoInput = document.querySelectorAll(".info-input");
 const infoEdit = document.querySelectorAll(".info-edit");
