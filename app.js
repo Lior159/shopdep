9ed963cb749ad8a8801de9db9eab450a3133963d
Lior Zalta <liorzalta24@gmail.com>
2023-05-22 22:46:19 +0300

    commit before deploy test

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use((req, res) => {
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    app.listen(3000);
+    app.listen(9090);
     console.log("connected");
   })
   .catch((err) => console.log(err));
